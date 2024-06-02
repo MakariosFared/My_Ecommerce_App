9ed8add56437d4ced37186b164ae2a066723acad
Makarios <makarios.fared@gmail.com>
2024-06-02 13:06:27 +0300

    chore: Refactor intro_screen_page_view widget to return a Column instead of a PageView.builder

--- lib/Features/introduction/data/models/intro_model.dart
+++ lib/Features/introduction/data/models/intro_model.dart
@@ -9,4 +9,21 @@ class IntroModel {
     required this.image,
   });
 }
-
+final item = [
+  IntroModel(
+    title: 'Delicious Deliveries, \nRight at Your Doorstep!',
+    description:
+        'Explore a World of Culinary Delights, \nDelivered Fast and Fresh!',
+    image: 'assets/images/intro_image_1.svg',
+  ),
+  IntroModel(
+    title: 'Delicious Deliveries,',
+    description: 'Explore a World of Culinary Delights,',
+    image: 'assets/images/intro_image_1.svg',
+  ),
+  IntroModel(
+    title: 'Delicious Deliveries,',
+    description: 'Explore a World of Culinary Delights,',
+    image: 'assets/images/intro_image_1.svg',
+  ),
+];
--- lib/Features/introduction/presentation/views/widgets/introduction_view_body.dart
+++ lib/Features/introduction/presentation/views/widgets/introduction_view_body.dart
@@ -1,9 +1,6 @@
-import 'package:dikkan/Core/utils/styles.dart';
-import 'package:dikkan/Core/utils/widgets/custom_button.dart';
 import 'package:dikkan/Features/introduction/data/models/intro_model.dart';
-import 'package:dikkan/Features/introduction/presentation/views/widgets/custom_dots.dart';
+import 'package:dikkan/Features/introduction/presentation/views/widgets/on_boarding_content.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class IntroductionViewBody extends StatefulWidget {
   const IntroductionViewBody({super.key});
@@ -12,24 +9,7 @@ class IntroductionViewBody extends StatefulWidget {
   State<IntroductionViewBody> createState() => _IntroductionViewBodyState();
 }
 
-final item = [
-  IntroModel(
-    title: 'Delicious Deliveries, \nRight at Your Doorstep!',
-    description:
-        'Explore a World of Culinary Delights, \nDelivered Fast and Fresh!',
-    image: 'assets/images/intro_image_1.svg',
-  ),
-  IntroModel(
-    title: 'Delicious Deliveries,',
-    description: 'Explore a World of Culinary Delights,',
-    image: 'assets/images/intro_image_1.svg',
-  ),
-  IntroModel(
-    title: 'Delicious Deliveries,',
-    description: 'Explore a World of Culinary Delights,',
-    image: 'assets/images/intro_image_1.svg',
-  ),
-];
+
 
 class _IntroductionViewBodyState extends State<IntroductionViewBody> {
   int selectedIndex = 0;
@@ -62,80 +42,4 @@ class _IntroductionViewBodyState extends State<IntroductionViewBody> {
   }
 }
 
-class OnBoardContent extends StatelessWidget {
-  const OnBoardContent({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.description,
-    required this.selectedIndex,
-  });
 
-  final String image, title, description;
-  final int selectedIndex;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 365,
-          width: 365,
-          child: SvgPicture.asset(image),
-        ),
-        const SizedBox(
-          height: 48,
-        ),
-        Container(
-          // color: Colors.white,
-          height: 331,
-          width: 365,
-          decoration: ShapeDecoration(
-            color: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(
-                  item.length,
-                  (index) => Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 4),
-                    child: CustomDots(isActive: selectedIndex == index),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 28,
-              ),
-              Text(
-                textAlign: TextAlign.center,
-                title,
-                // 'Delicious Deliveries, \nRight at Your Doorstep!',
-                style: Styles.textStyle24.copyWith(color: Colors.black),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Text(
-                textAlign: TextAlign.center,
-                description,
-                // 'Explore a World of Culinary Delights, \nDelivered Fast and Fresh!',
-                style: Styles.textStyle16.copyWith(color: Colors.black),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              const CustomButton(
-                text: 'Get Started',
-              )
-            ],
-          ),
-        )
-      ],
-    );
-  }
-}
