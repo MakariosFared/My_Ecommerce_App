c565f4fb831a8c2b39fdf5c252b80dc3ec0fbd8b
Makarios <makarios.fared@gmail.com>
2024-07-01 19:35:49 +0300

    responsive auth header

--- lib/Features/auth/presentation/views/widgets/auth_header.dart
+++ lib/Features/auth/presentation/views/widgets/auth_header.dart
@@ -8,23 +8,28 @@ class AuthHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: MediaQuery.of(context).size.height * 0.34,
-      decoration: const BoxDecoration(
-        color: kGreenColor,
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(25),
-          bottomRight: Radius.circular(25),
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.3,
+          decoration: const BoxDecoration(
+            color: kGreenColor,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(25),
+              bottomRight: Radius.circular(25),
+            ),
+          ),
         ),
-      ),
-      child: Image.asset(
-        alignment: Alignment.bottomCenter,
-        'assets/images/Group 31.png',
-        // AssetsData.logo,
-        height: MediaQuery.of(context).size.height * 0.34 * 0.59,
-        width: 314,
-      ),
+        Image.asset(
+          alignment: Alignment.bottomCenter,
+          'assets/images/Group 31.png',
+          // AssetsData.logo,
+          height: MediaQuery.of(context).size.height * 0.24,
+          // width: MediaQuery.of(context).size.width * 0.9,
+        ),
+      ],
     );
   }
 }
