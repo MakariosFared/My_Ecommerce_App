db9f0bc1b34075232690f15af2e9f05c10031e82
Makarios <makarios.fared@gmail.com>
2024-07-04 21:52:33 +0300

    popular categories item

--- lib/Features/search/presentation/views/widget/popular_categories.dart
+++ lib/Features/search/presentation/views/widget/popular_categories.dart
@@ -1,4 +1,5 @@
 import 'package:dikkan/Core/utils/styles.dart';
+import 'package:dikkan/Features/search/presentation/views/widget/popular_categories_item.dart';
 import 'package:flutter/material.dart';
 
 class PopularCategories extends StatelessWidget {
@@ -18,56 +19,11 @@ class PopularCategories extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          PopularCategoriesCard(),
+          PopularCategoriesItem(),
         ],
       ),
     );
   }
 }
 
-class PopularCategoriesCard extends StatelessWidget {
-  const PopularCategoriesCard({
-    super.key,
-  });
 
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        Container(
-          height: 119,
-          width: 119,
-          decoration: BoxDecoration(
-            // color: Colors.red,
-            borderRadius: BorderRadius.circular(12),
-            image: const DecorationImage(
-              image: AssetImage('assets/images/vegetables.png'),
-              fit: BoxFit.fill,
-            ),
-          ),
-        ),
-        Container(
-          height: 34,
-          width: 119,
-          decoration: const BoxDecoration(
-            color: Color(0xff25418B),
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(12),
-              bottomRight: Radius.circular(12),
-            ),
-          ),
-          child: Center(
-            child: Text(
-              'Pizza',
-              style: Styles.textStyleSfProDisplayRegular12.copyWith(
-                color: Colors.white,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
---
+++ lib/Features/search/presentation/views/widget/popular_categories_item.dart
@@ -0,0 +1,49 @@
+import 'package:dikkan/Core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class PopularCategoriesItem extends StatelessWidget {
+  const PopularCategoriesItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        Container(
+          height: 119,
+          width: 119,
+          decoration: BoxDecoration(
+            // color: Colors.red,
+            borderRadius: BorderRadius.circular(12),
+            image: const DecorationImage(
+              image: AssetImage('assets/images/vegetables.png'),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        Container(
+          height: 34,
+          width: 119,
+          decoration: const BoxDecoration(
+            color: Color(0xff25418B),
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(12),
+              bottomRight: Radius.circular(12),
+            ),
+          ),
+          child: Center(
+            child: Text(
+              'Pizza',
+              style: Styles.textStyleSfProDisplayRegular12.copyWith(
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
