c0a57e1c50c28fdcd2dc9738a0ef207b393ce4b0
Makarios <makarios.fared@gmail.com>
2024-07-16 20:35:43 +0300

    ongoing body

--- lib/Features/orders/presentation/views/orders_view.dart
+++ lib/Features/orders/presentation/views/orders_view.dart
@@ -1,9 +1,7 @@
 import 'package:dikkan/Core/utils/styles.dart';
-import 'package:dikkan/Core/utils/widgets/custom_button.dart';
+import 'package:dikkan/Features/orders/presentation/views/widgets/ongoing_body.dart';
 import 'package:dikkan/constant.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/svg.dart';
 
 class OrdersView extends StatefulWidget {
   const OrdersView({super.key});
@@ -65,57 +63,7 @@ class _OrdersViewState extends State<OrdersView>
       body: TabBarView(
         controller: tabController,
         children: [
-          Column(
-            children: [
-              Flexible(
-                child: Center(
-                  child: Opacity(
-                    opacity: 0.5,
-                    child: SvgPicture.asset(
-                      'assets/images/ongoing Photo.svg',
-                      colorFilter: const ColorFilter.mode(
-                        Colors.white,
-                        BlendMode.saturation,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                'Ongoing is Empty',
-                style: Styles.textStyleSfProDisplayMedium16.copyWith(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 10,
-                  bottom: 40,
-                ),
-                child: Text(
-                  'You can go to Discover to add products.',
-                  style: Styles.textStyleSfProDisplayRegular15.copyWith(
-                    color: const Color(0xffACB1C0),
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: 170,
-                child: CustomButton(
-                  borderRadius: BorderRadius.circular(10),
-                  text: 'Go Discover',
-                  onPressed: () {},
-                  style: Styles.textStyleSfProDisplayRegular15.copyWith(
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 70),
-            ],
-          ),
+          const OnGoingBody(),
           Center(
             child: Text(
               'history',
@@ -133,3 +81,5 @@ class _OrdersViewState extends State<OrdersView>
     );
   }
 }
+
+
---
+++ lib/Features/orders/presentation/views/widgets/ongoing_body.dart
@@ -0,0 +1,65 @@
+import 'package:dikkan/Core/utils/styles.dart';
+import 'package:dikkan/Core/utils/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class OnGoingBody extends StatelessWidget {
+  const OnGoingBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Flexible(
+          child: Center(
+            child: Opacity(
+              opacity: 0.5,
+              child: SvgPicture.asset(
+                'assets/images/ongoing Photo.svg',
+                colorFilter: const ColorFilter.mode(
+                  Colors.white,
+                  BlendMode.saturation,
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          'Ongoing is Empty',
+          style: Styles.textStyleSfProDisplayMedium16.copyWith(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            top: 10,
+            bottom: 40,
+          ),
+          child: Text(
+            'You can go to Discover to add products.',
+            style: Styles.textStyleSfProDisplayRegular15.copyWith(
+              color: const Color(0xffACB1C0),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 170,
+          child: CustomButton(
+            borderRadius: BorderRadius.circular(10),
+            text: 'Go Discover',
+            onPressed: () {},
+            style: Styles.textStyleSfProDisplayRegular15.copyWith(
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        const SizedBox(height: 70),
+      ],
+    );
+  }
+}
