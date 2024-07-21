850967934f5a9071adcd1f08802537e5fe0c2e47
Makarios <makarios.fared@gmail.com>
2024-07-21 16:43:36 +0300

    my favorite item model

---
+++ lib/Features/my_favorites/data/models/my_favorite_item_model.dart
@@ -0,0 +1,11 @@
+class MyFavoriteItemModel {
+  final String productName;
+  final String shopName;
+  final double price;
+
+  MyFavoriteItemModel({
+    required this.productName,
+    required this.shopName,
+    required this.price,
+  });
+}
