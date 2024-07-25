8489f898a425f756d13bbd7dc2f1955457870fe6
Makarios <makarios.fared@gmail.com>
2024-07-25 19:24:31 +0300

    details header bar

--- lib/Features/home/presentation/views/details_view.dart
+++ lib/Features/home/presentation/views/details_view.dart
@@ -1,3 +1,4 @@
+import 'package:dikkan/constant.dart';
 import 'package:flutter/material.dart';
 
 class DetailsView extends StatelessWidget {
@@ -5,21 +6,108 @@ class DetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DetailsView'),
-      ),
-      body: const DetailsViewBody(),
+    return const Scaffold(
+      body: DetailsViewBody(),
     );
   }
 }
-
 
 class DetailsViewBody extends StatelessWidget {
   const DetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Stack(
+          fit: StackFit.passthrough,
+          children: [
+            Image.asset(
+              height: MediaQuery.of(context).size.height * 0.39,
+              'assets/images/fresh orange.png',
+              fit: BoxFit.fill,
+            ),
+            const DetailsHeaderBar(),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class DetailsHeaderBar extends StatelessWidget {
+  const DetailsHeaderBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 50,
+        left: 24,
+        right: 24,
+      ),
+      child: Row(
+        children: [
+          Container(
+            height: 40,
+            width: 40,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: IconButton(
+              color: Colors.black,
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+          const Spacer(),
+          Container(
+            height: 40,
+            width: 40,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: IconButton(
+              color: Colors.black,
+              icon: const Icon(
+                Icons.favorite,
+                color: kGreenColor,
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+          const SizedBox(
+            width: 16,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: IconButton(
+              color: Colors.black,
+              icon: const Icon(
+                Icons.more_vert,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/Features/home/presentation/views/widgets/popular_grid_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/popular_grid_list_view_item.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
 
+import 'package:dikkan/Core/utils/app_router.dart';
 import 'package:dikkan/Core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class PopularGridListViewItem extends StatelessWidget {
   const PopularGridListViewItem({
@@ -10,33 +12,38 @@ class PopularGridListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 92,
-      height: 118,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            Image.asset(
-              'assets/images/fresh orange.png',
-              fit: BoxFit.cover,
-            ),
-            BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-              child: Container(),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                ' Drow\nRanger',
-                style: Styles.textStyleSfProDisplayRegular15.copyWith(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kDetailsView);
+      },
+      child: SizedBox(
+        width: 92,
+        height: 118,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.asset(
+                'assets/images/fresh orange.png',
+                fit: BoxFit.cover,
+              ),
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 1.6, sigmaY: 1.6),
+                child: Container(),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  ' Drow\nRanger',
+                  style: Styles.textStyleSfProDisplayRegular15.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
