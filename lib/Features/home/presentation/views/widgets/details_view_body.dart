06c4049fc749f9d551b8f744cd6e283ca76eb567
Makarios <makarios.fared@gmail.com>
2024-07-28 20:11:07 +0300

    refactor: Update DetailsView to use DetailsViewBody widget

---
+++ lib/Features/home/presentation/views/widgets/details_view_body.dart
@@ -0,0 +1,66 @@
+import 'package:dikkan/Features/home/presentation/views/widgets/details_header_bar.dart';
+import 'package:dikkan/Features/home/presentation/views/widgets/details_product_images_section.dart';
+import 'package:dikkan/Features/home/presentation/views/widgets/dots_indicator.dart';
+import 'package:flutter/material.dart';
+
+class DetailsViewBody extends StatefulWidget {
+  const DetailsViewBody({super.key});
+
+  @override
+  State<DetailsViewBody> createState() => _DetailsViewBodyState();
+}
+
+class _DetailsViewBodyState extends State<DetailsViewBody> {
+  late PageController pageController;
+  int currentPageIndex = 0;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    pageController.addListener(() {
+      currentPageIndex = pageController.page!.round();
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Stack(
+          fit: StackFit.passthrough,
+          children: [
+            DetailsProductImagesSection(
+              pageController: pageController,
+            ),
+            const DetailsHeaderBar(),
+            Positioned(
+              bottom: 16,
+              right: 24,
+              child: DotsIndicator(currentPageIndex: currentPageIndex),
+            ),
+          ],
+        ),
+        Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          height: 124,
+          child: const Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Fresh Orange',
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
