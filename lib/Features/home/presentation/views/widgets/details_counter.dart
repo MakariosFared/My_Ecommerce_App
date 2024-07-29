6c001af713890fb8268b73c8bb42817ccbe45186
Makarios <makarios.fared@gmail.com>
2024-07-29 16:12:31 +0300

    details counter

---
+++ lib/Features/home/presentation/views/widgets/details_counter.dart
@@ -0,0 +1,68 @@
+import 'package:dikkan/Core/utils/widgets/custom_counter_button.dart';
+import 'package:dikkan/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DerailsCounter extends StatefulWidget {
+  const DerailsCounter({super.key});
+
+  @override
+  DerailsCounterState createState() => DerailsCounterState();
+}
+
+class DerailsCounterState extends State<DerailsCounter> {
+  int counter = 0; 
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CustomCounterButton(
+          onPressed: () {
+            setState(
+              () {
+                if (counter > 0) {
+                  counter -= 1;
+                }
+              },
+            );
+          },
+          icon: const Icon(
+            Icons.remove,
+            size: 20,
+            color: Color(0xff1C304F),
+          ),
+        ),
+        const SizedBox(
+          width: 16,
+        ),
+        Text(
+          '$counter',
+          style: GoogleFonts.poppins(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: kGreenColor,
+          ),
+        ),
+        const SizedBox(
+          width: 16,
+        ),
+        CustomCounterButton(
+          onPressed: () {
+            setState(
+              () {
+                counter += 1;
+              },
+            );
+          },
+          icon: const Icon(
+            Icons.add,
+            size: 20,
+            color: Color(0xff1C304F),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/details_view_body.dart
@@ -1,8 +1,7 @@
-import 'package:dikkan/Core/utils/widgets/custom_counter_button.dart';
+import 'package:dikkan/Features/home/presentation/views/widgets/details_counter.dart';
 import 'package:dikkan/Features/home/presentation/views/widgets/details_header_bar.dart';
 import 'package:dikkan/Features/home/presentation/views/widgets/details_product_images_section.dart';
 import 'package:dikkan/Features/home/presentation/views/widgets/dots_indicator.dart';
-import 'package:dikkan/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -115,7 +114,7 @@ class _DetailsViewBodyState extends State<DetailsViewBody> {
                     ),
                   ),
                   const Spacer(),
-                  const MyCounter(),
+                  const DerailsCounter(),
                 ],
               )
             ],
@@ -126,66 +125,4 @@ class _DetailsViewBodyState extends State<DetailsViewBody> {
   }
 }
 
-class MyCounter extends StatefulWidget {
-  const MyCounter({super.key});
 
-  @override
-  MyCounterState createState() => MyCounterState();
-}
-
-class MyCounterState extends State<MyCounter> {
-  int counter = 0; // العدد الابتدائي
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        CustomCounterButton(
-          onPressed: () {
-            setState(
-              () {
-                if (counter > 0) {
-                  counter -= 1;
-                }
-              },
-            );
-          },
-          icon: const Icon(
-            Icons.remove,
-            size: 20,
-            color: Color(0xff1C304F),
-          ),
-        ),
-        const SizedBox(
-          width: 16,
-        ),
-        Text(
-          '$counter',
-          style: GoogleFonts.poppins(
-            fontSize: 20,
-            fontWeight: FontWeight.w600,
-            color: kGreenColor,
-          ),
-        ),
-        const SizedBox(
-          width: 16,
-        ),
-        CustomCounterButton(
-          onPressed: () {
-            setState(
-              () {
-                counter += 1;
-              },
-            );
-          },
-          icon: const Icon(
-            Icons.add,
-            size: 20,
-            color: Color(0xff1C304F),
-          ),
-        ),
-      ],
-    );
-  }
-}
